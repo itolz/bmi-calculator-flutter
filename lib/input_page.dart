0de7f62e71133f23c7955cdc83658ab68d7faa07
Italo Vinicios <italovinicios@gmail.com>
2020-01-27 21:34:46 -0300

    construtor required

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,16 +16,16 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
             Expanded(child: Row(
               children: <Widget>[
-                Expanded(child: ReusableCard(),),
-                Expanded(child: ReusableCard(),),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),),),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),),),
               ],
             ),
             ),
-            Expanded(child: ReusableCard(),),
+            Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),),),
             Expanded(child: Row(
               children: <Widget>[
-                Expanded(child: ReusableCard(),),
-                Expanded(child: ReusableCard(),),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),),),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),),),
               ],
             ),
             ),
@@ -36,16 +36,19 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key key,
-  }) : super(key: key);
+
+
+  Color colour;
+
+  ReusableCard({@required this.colour});
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
     margin: EdgeInsets.all(15.0),
     decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colour,
         borderRadius: BorderRadius.circular(10.0)),
               );
   }
