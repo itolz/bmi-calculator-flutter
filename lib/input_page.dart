00015bf03a77ea27b93ba08cc7465f18a54d6976
Italo Vinicios <italovinicios@gmail.com>
2020-02-18 20:49:34 -0300

    implementado slide dinamico

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -94,7 +94,9 @@ class _InputPageState extends State<InputPage> {
                     activeColor: Color(0xFFEB1555),
                     inactiveColor: Color(0XFF8D8E98),
                     onChanged: (double newValue) {
-                      print(newValue);
+                      setState(() {
+                      height = newValue.round();
+                      });
                     },
                   ),
                 ],
