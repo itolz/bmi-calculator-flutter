325ffbb6ab2f4c1c43adcbf3e455f25fcfd416a8
itolz <italovinicios@gmail.com>
2020-02-19 21:05:09 -0300

    finalizado mais alguns ajustes no theme do slider

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -91,6 +91,7 @@ class _InputPageState extends State<InputPage> {
                     data: SliderTheme.of(context).copyWith(
                       thumbColor: Color(0xFFEB1555),
                       activeTrackColor: Colors.white,
+                      inactiveTrackColor: Color(0XFF8D8E98),
                       overlayColor: Color(0x1fEB1555),
                       thumbShape:
                           RoundSliderThumbShape(enabledThumbRadius: 15.0),
@@ -101,7 +102,6 @@ class _InputPageState extends State<InputPage> {
                       value: height.toDouble(),
                       min: 0,
                       max: 100,
-                      inactiveColor: Color(0XFF8D8E98),
                       onChanged: (double newValue) {
                         setState(() {
                           height = newValue.round();
